45e4ea1ad23be085fb222ce5da085302919254f7
woo37830 <jwooten37830@me.com>
2020-02-14 13:56:07 -0500

    Modified so that tabs don't work if not logged in

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <html>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script type="text/javascript" src="./_js/jquery.easyui.min.js"></script>
-<link rel="stylesheet" href="_css/layout.css" id="styleid" type="text/css" />
-<link rel="stylesheet" href="_css/dlg-form.css" id="formid" type="text/css" />
-<link rel="stylesheet" type="text/css" href="./themes/default/easyui.css">
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="./_js/jquery.easyui.min.js"></script>
+  <script type="text/javascript" src="./_js/cookies.js"></script>
+  <script type="text/javascript" src="./_js/authorize.js"></script>
+  <link rel="stylesheet" href="_css/layout.css" id="styleid" type="text/css" />
+  <link rel="stylesheet" href="_css/dlg-form.css" id="formid" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="./themes/default/easyui.css">
 </head>
 <body>
   <!-- Tab links -->
@@ -31,33 +32,35 @@
   <div id="no-access">
     <center><h3>You must be logged in to Browse the Journal</h3></center>
   </div>
- <div id="data">
-   <!-- Tab content -->
-   <div id="Years" class="tabcontent">
-     <center><h3>Journal Entries</h3></center>
-<ul>
-<?php
-  $NOTE_DIR="/Users/woo/Dropbox/Personal/Documents/Notes/";
+ <?php
+ require 'login.php';
+ echo "$scripts";
+ $page = "<div id='page'>";
+ $tab1 =   "<div id='Years' class='tabcontent'>";
+ $data1 = "<div class='data'>";
+ $data1 .= "<center><h3>Journal Entries</h3></center>";
 
+ $data1 .= "<ul>";
+
+  $NOTE_DIR="/Users/woo/Dropbox/Personal/Documents/Notes/";
 if ($handle = opendir($NOTE_DIR) ) {
     $files = glob($NOTE_DIR . "Journal_*.txt");
     foreach( $files as $file) {
     $filename = basename($file, ".txt");
     $year = substr($filename, -4);
     // extract filename and show with link to page with full filepath
-    echo "<li><a href='./page.php?year=$year'>$filename</a></li>";
+    $data1 .= "<li><a href='./page.php?year=$year'>$filename</a></li>";
     }
 closedir($handle);
 }
-echo "</ul>";
+$data1 .= "</ul>";
+$data1 .= "</div>";
+$tab1 .= $data1 . "</div>";// end data
 
-echo "</div>"; // end of journal entries
+$tab2 = "<div id='Latest' class='tabcontent' >";
 
-echo "<p />";
-?>
-<div id="Latest" class="tabcontent" >
-<?php
-echo "<center><h2>Recent Notes</h2></center><br/>";
+$data2 = "<div class='data'>";
+$data2 .= "<center><h2>Recent Notes</h2></center><br/>";
 $YYYY=date('Y');
 $source = "$NOTE_DIR"."Journal_$YYYY.txt";
 $raw = file_get_contents($source);
@@ -69,14 +72,12 @@ for( $i = $start; $i <= $num; $i++ ) {
     $raw .= $lines[$i] . "\n";
 }
 $html = nl2br($raw);
-$output = "<div>$html</div>";
-echo "$output";
-echo "<p />";
-echo "</div>"; //end of recent notes
-?>
-<div id="To Do" class="tabcontent">
-<?php
-echo "<center><h2>ToDo List</h2></center><br/>";
+$data2 .= $html;
+$data2 .= "</div>"; // end data
+$tab2 .= $data2 . "</div>";
+$tab3 = "<div id='To Do' class='tabcontent'>";
+$data3 = "<div class='data'>";
+$data3 .= "<center><h2>ToDo List</h2></center><br/>";
 $source = "$NOTE_DIR".".todo";
 $raw = file_get_contents($source);
 $lines = preg_split("/\n/",$raw);
@@ -87,25 +88,16 @@ for( $i = $start; $i <= $num; $i++ ) {
     $raw .= $lines[$i] . "\n";
     }
 $html = nl2br($raw);
-$output = "<div>$html</div>";
-echo "$output";
-echo "</div>";
+$data3 .= $html;
+$data3 .= "</div>";
+$tab3 .= $data3 . "</div>";
+echo "$page";
+echo "$tab1";
+echo "$tab2";
+echo "$tab3";
+echo "</div>"; // end of page
 ?>
 <script type="text/javascript">
-$(document).on('click','#log_in', function() {
-$.getJSON( "./users.txt", function( json ) {
-     $.each(json, function( key, value) {
-     //for( var i=0; i<value.length; i++ ) {
-    //alert("user = "+value[i].user+" password = "+value[i].passwd);
-    //}
-    users = $.map(value, function(el) { return el });
-});
-});
-    login();
-});
-$(document).on('click','#log_out', function() {
-    logout();
-});
 
 document.getElementById("defaultOpen").click();
 
@@ -124,92 +116,27 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  if( readCookie("logged_in") ) {
+                  $("#log_in").text("Logout: "+readCookie('userid'));
+                  $("#log_in").attr('id', 'log_out');
+                    $(".data").attr('class', 'show');
+                    $("#no-access").attr('class','hide');
+                    $("#lin").attr('class','hide');
+                    $("#lin-buttons").attr('class','hide');
+
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+} else {
+            $("#log_out").text("Login");
+            $("#log_out").attr('id', 'log_in');
+              $(".data").attr('class', 'hide');
+              $("#no-access").attr('class','show');
+}
 }
 
-        if( readCookie("logged_in") ) {
-                        $("#log_in").text("Logout: "+readCookie('userid'));
-                        $("#log_in").attr('id', 'log_out');
-                          $("#data").attr('class', 'show');
-                          $("#no-access").attr('class','hide');
-                          $("#lin").attr('class','hide');
-                          $("#lin-buttons").attr('class','hide');
-        } else {
-                    $("#log_out").text("Login");
-                    $("#log_out").attr('id', 'log_in');
-                      $("#data").attr('class', 'hide');
-                      $("#no-access").attr('class','show');
-      }
-               var days = 1;
-            function login() {
-                        $('#lin').dialog('open').dialog('set Title', 'Login');
-                        $('#lfm').form('clear');
-                }
-            function logout() {
-                    eraseCookie('logged_in');
-                    eraseCookie('userid');
-                    window.location.reload();
-                }
-            function authorize() {
-                // Compare the form data to the json data and if match, createCookie, else logout;
-                // alert("json data: " + json.user[1] + ", " + json.passwd);
-                //alert("form data: " + document.getElementById("userid").value);
-                uName = document.getElementById("userid").value;
-                var pName = document.getElementById("password").value;
-               $('#lin').dialog('close');
-               var done = false;
-             for( var i=0;i<users.length&&!done;i++) {
-                 // alert("Testing '"+users[i].user+"' and '"+users[i].passwd+"'");
-                    if( uName == users[i].user && pName == users[i].passwd ) {
-                        createCookie('logged_in', 'yes', days);
-                        createCookie('userid', uName);
-                        user = uName;
-                        done = true;
-                    }
-                }
-                if( !done ) {
-                        alert('username or password not found');
-                        logout();
-                } else {
-                   // alert("Cookie: 'logged_in': "+readCookie('logged_in'));
-                    }
-                    window.location.reload();
-                }
-                // Cookie functions
-              function createCookie(name,value,days) {
-                if (days) {
-                  var date = new Date();
-                  date.setTime(date.getTime()+(days*24*60*60*1000));
-                  var expires = "; expires="+date.toGMTString();
-                }
-                else var expires = "";
-                document.cookie = name+"="+value+expires+"; path=/";
-              }
 
-              function readCookie(name) {
-                var nameEQ = name + "=";
-                var ca = document.cookie.split(';');
-                for(var i=0;i < ca.length;i++) {
-                  var c = ca[i];
-                  while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                  if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-                }
-                return null;
-              }
-
-              function eraseCookie(name) {
-                createCookie(name,"",-1);
-                //alert("Cookie: "+name+": "+readCookie(name));
-                  }
-                var logged_in = document.getElementById('logged_in');
-                if (readCookie('logged_in') ) {
-                    createCookie('logged_in', 'yes', days);
-                } else {
-                    toolbar.className = 'hide';
-                }
 </script>
 
 <div id="footer" >
@@ -220,24 +147,5 @@ function openCity(evt, cityName) {
 </div>
 </div>
 </div>
-<div id="lin" class="easyui-dialog" style="width:400px;height:380px;padding:10px 20px"
-    closed="true" buttons="#lin-buttons">
-  <div class="ftitle">Login</div>
-  <form id="lfm" method="post" novalidate>
-    <div class="fitem">
-      <label>User:</label>
-      <input name="user" class="easyui-textbox" id="userid" required="true" autofocus>
-    </div>
-    <div class="fitem">
-      <label>Password:</label>
-      <input name="passwd" type= "password" class="easyui-textbox" id="password" required="true">
-    </div>
-  </form>
-</div>
-<div id="lin-buttons">
-  <a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="authorize()" style="width:90px">Login</a>
-  <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#lin').dialog('close')" style="width:90px">Cancel</a>
-</div>
-
 </body>
 </html>
