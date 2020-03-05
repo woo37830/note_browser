bad11a84eeadb829eb107c026328b60ee0270bed
woo37830 <jwooten37830@me.com>
2020-03-04 19:02:34 -0500

    ToDo add and delete, Latest add, need cleanup of url and goto selected search line

--- index.php
+++ index.php
@@ -75,6 +75,19 @@ $data2 = "<div class='data'>";
 $data2 .= "<center><h2>Recent Notes</h2></center><br/>";
 $YYYY=date('Y');
 $source = "$NOTE_DIR"."Journal_$YYYY.txt";
+if( !empty($_REQUEST['diary']) ) {
+  $raw = file_get_contents($source);
+  $lines = preg_split("/\n/",$raw);
+  $num = count($lines);
+  $file = fopen($source, 'w');
+  for( $i = 0; $i <= $num; $i++ ) {
+      fwrite($file, "\n" . $lines[$i] );
+  }
+  $datestr = date('Ymd h:m:s');
+  fwrite($file, "\n---$datestr");
+  fwrite($file, "\n" . $_REQUEST['diary']);
+  fclose($file);
+}
 $raw = file_get_contents($source);
 $lines = preg_split("/\n/",$raw);
 $num = count($lines);
@@ -83,6 +96,8 @@ $raw = "";
 for( $i = $start; $i <= $num; $i++ ) {
     $raw .= $lines[$i] . "\n";
 }
+$raw .= "<br /><hr /><br /><form action='./index.php' ><input type='textarea' name='diary' value='' cols='50' rows='10' /><input type='submit' value='Add' /><input type='submit' value='Cancel' /></form>";
+
 $html = nl2br($raw);
 $data2 .= $html;
 $data2 .= "</div>"; // end data
