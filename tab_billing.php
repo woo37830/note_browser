6241b760fc92ad529fcde98da84952e1d8ee4a03
woo37830 <jwooten37830@me.com>
2020-03-07 11:50:08 -0500

    Finished showing billed hours

--- tab_billing.php
+++ tab_billing.php
@@ -25,12 +25,17 @@
  $task = $parsed[2];
  $client = $parsed[3];
  $num = $parsed[4];
+ $total = $parsed[6];
  $last_line = "<h3>Billing</h3>";
  if( $end ) {
    $last_line .= "No one.";
  } else {
-   $last_line = $client . " for  '" . $task . "' since " . $start ;
+   $last_line .= $client . " for  '" . $task . "' since " . $start ;
  }
+ if( $total ) {
+   $last_line .= "<br />Total Billed: $total";
+ }
+ $last_line .=
  $html = nl2br($last_line);
  $data5 .= $html;
  $raw = $parsed[5];
@@ -48,25 +53,96 @@
  function turn_off($source, $date_str) {
     return "Turned off at: $date_str";
  }
+ function get_yearMonDayHourMin( $time_str ) {
+   //echo "<br />time_str: $time_str";
+   $time = explode(' ', $time_str); // gives mm/dd/yyyy
+   $pieces = explode('/', $time[0]);
+   $hhmm = explode(':', $time[1]);
+   //echo "<br />ymdhm = $pieces[2],$pieces[0],$pieces[1],$hhmm[0],$hhmm[1]";
+   return array($pieces[2],$pieces[0],$pieces[1],$hhmm[0],$hhmm[1]);
+ }
+ function get_dates( $line ) {
+   // return start, end, (start_ymdhm, end_ymdhm), task, client
+   $pieces = explode(') ', $line); // gives times, rest of string
+   $time_str = $pieces[0]; // start : end
+
+   $time_str = substr($time_str, 1); // strip off the leading ()
+   $times = explode(' - ', $time_str);
+   $start = $times[0];
+   $end = $times[1];
+
+   $start_ymdhm = get_yearMonDayHourMin( $start );
+   $end_ymdhm = get_yearMonDayHourMin( $end );
+   $task_client = explode('@', $pieces[1]); // task @ client
+   $task = $task_client[0];
+   $client = $task_client[1];
+   return array($start, $end, $start_ymdhm, $end_ymdhm, $task, $client );
+ }
+ function get_billed($start, $end) {
+   $date1 = new DateTime($start);
+   if( empty( $end ) ) {
+     $date2 = new DateTime();
+   } else {
+     $date2 = new DateTime( $end );
+   }
+   $diff = $date2->diff($date1);
+   $ret =  60*((24*$diff->d)+$diff->h)+$diff->i;
+   //echo "<br />$start, $end, $ret";
+   return $ret;
+ }
+ function get_todays_billed($lines) {
+      $num = count($lines);
+      $year = date('Y');
+      $mon = date('m');
+      $day = date('d');
+      //echo "<br/>year: $year, mon: $mon, day: $day";
+      $ret = "<br/><table><tr><th>Client</th><th>Billed</th></tr>";
+      $billing = array();
+      for( $i = $num-1; $i >= $num-3; $i-- ) {
+        $dates = get_dates( $lines[$i] ); // return Y, m, d in array followed by start, end
+        $start_ymdhm = $dates[2];
+        $start_month = $start_ymdhm[1];
+        $start_day = $start_ymdhm[2];
+        $start_year = $start_ymdhm[0];
+        if( $start_year == $year && $start_month == $mon && $start_day == $day ) {
+          $client = $dates[5];
+          if( empty($billing[$client]) ) {
+            $billing[$client] = 0;
+          }
+          $billing[$client] += get_billed($dates[0], $dates[1]);
+          }
+      }
+      $total = 0;
+      foreach( $billing as $aclient=>$value ) {
+        if( $value < 3 ) {
+          $value = "Not Much";
+        }
+        $total += $value;
+        $ret .= "<tr><td>" . $aclient . "</th><td>" . $value . "</td></tr>";
+      }
+      $ret .= "<tr><td>Total</td><td>$total</td></tr></table>";
+      return $ret;
+ }
  function parse_last_line($source) {
    $raw = file_get_contents($source);
    $lines = preg_split("/\n/",$raw);
    $num = count($lines);
    // Check if last line has no end time
-   $pieces = explode(') ', $lines[$num-1]); // gives times, rest of string
-   $time_str = $pieces[0]; // start : end
-   $time_str = substr($time_str, 1); // strip off the leading ()
-   $times = explode(' - ', $time_str);
-   $start = $times[0];
-   $end = $times[1];
-   $pieces = explode('@', $pieces[1]); // task @ client
-   $task = $pieces[0];
-   $client = $pieces[1];
+   $dates = get_dates( $lines[$num-1]);
+   $start_ymdhm = $dates[2];
+   //echo "<br />get_dates: $dates[0], $dates[1], $start_ymdhm[0], $start_ymdhm[1], $start_ymdhm[2], $start_ymdhm[3] ";
+   $start = $dates[0];
+   $end = $dates[1];
+   $task = $dates[4];
+   $client = $dates[5];
+   // Get totals for today
+   $total = get_todays_billed($lines);
+   die("<br />$total<br />All Done");
    $begin = $num -10;
    $raw = "<h3>Recent Actions</h3><br />";
    for( $i = $begin; $i <= $num; $i++ ) {
        $raw .= $lines[$i] . "\n";
    }
-   return array($start, $end, $task, $client, $num, $raw);
+   return array($start, $end, $task, $client, $num, $raw, $total);
  }
  ?>
